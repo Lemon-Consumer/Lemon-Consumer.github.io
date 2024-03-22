7d2b1ea1b690083657614136cf5bd6577d4a66d1
Lemon-Consumer <102989249+Lemon-Consumer@users.noreply.github.com>
2024-03-22 10:25:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const off = document.querySelector("#off");
 
 const turnOff = () => {
-  
+  let text = document.querySelectorAll('p', 'li');
+  if (text.visibility === 'hidden') {
+    text.visibility = 'visible';
+  } else {
+    text.visibility = 'hidden';
+  }
 }
+
+off.addEventListener('click', turnOff);
